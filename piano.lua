feef574c190a3e38581cce27524736862b4e6bae
aousqr <107679368+aousqr@users.noreply.github.com>
2022-06-17 17:44:46 +0800

    Create piano.lua

---
+++ piano.lua
@@ -0,0 +1,7 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 9*60 + 25 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- tempo overides the delay
+
+shared.scr = [[
